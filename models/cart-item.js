12ecc1a900137a3199104653ba29540353919515
Renato Spakauskas <renatospaka@gmail.com>
2020-08-31 12:49:21 -0300

    Atualizando para o desenvolvimento atual

--- models/cart-item.js
+++ models/cart-item.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 
-const sequelize = require('../util/database');
+const sequelize = require('../util/database')
 
 const CartItem = sequelize.define('cartItem', {
   id: {
@@ -10,6 +10,6 @@ const CartItem = sequelize.define('cartItem', {
     primaryKey: true
   },
   quantity: Sequelize.INTEGER
-});
+})
 
-module.exports = CartItem;
+module.exports = CartItem
--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const sequelize = new Sequelize(
+  'node_complete', // database
+  'nodecomplete', // user
+  'nodecomplete', // pwd,
+  {
+    dialect: 'mysql',
+    host: 'hkh-mysql-d.co7iaamhjk2r.us-east-2.rds.amazonaws.com',
+    port: 3306
+  })
 
-module.exports = sequelize;
+module.exports = sequelize
