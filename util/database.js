8545584cf168bb3bd53a306e6c7f05f6d0695303
Renato Spakauskas <renatospaka@gmail.com>
2020-08-31 22:56:48 -0300

    Conectando ao mongodb (Atlas)

--- util/database.js
+++ util/database.js
@@ -1,13 +1,28 @@
-const Sequelize = require('sequelize')
+// const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(
-  'node_complete', // database
-  'nodecomplete', // user
-  'nodecomplete', // pwd,
-  {
-    dialect: 'mysql',
-    host: 'hkh-mysql-d.co7iaamhjk2r.us-east-2.rds.amazonaws.com',
-    port: 3306
-  })
+// const sequelize = new Sequelize(
+//   'node_complete', // database
+//   'nodecomplete', // user
+//   'nodecomplete', // pwd,
+//   {
+//     dialect: 'mysql',
+//     host: 'hkh-mysql-d.co7iaamhjk2r.us-east-2.rds.amazonaws.com',
+//     port: 3306
+//   })
 
-module.exports = sequelize
+// module.exports = sequelize
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://nodecomplete:nodecomplete@renatospaka-m001-oiyd7.mongodb.net/dev_bs?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected!')
+      callback(client)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+module.exports = mongoConnect;
