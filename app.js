f607aa9e763e144bea88d72ff0c9af78ab3f9083
Renato Spakauskas <renatospaka@gmail.com>
2020-09-02 11:58:12 -0300

    Trabalhando com usuários (model users)

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error')
-const mongoConnect = require('./util/database').mongoConnect
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user
-  //     next()
-  //   })
-  //   .catch(err => console.log(err))
-  next()
-})
+  User.findById('5f4f9d19261eea577a111b96')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404)
+app.use(errorController.get404);
 
 mongoConnect(() => {
-  app.listen(3000)
-})
+  app.listen(3000);
+});
--- models/product.js
+++ models/product.js
@@ -3,25 +3,26 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectID(id) : null;
+    this.userId = userId;
   }
 
   save() {
-    const db = getDb()
-    let dbOp
+    const db = getDb();
+    let dbOp;
     if (this._id) {
       // update the product
       dbOp = db.collection('products')
-        .updateOne({ _id: this._id }, { $set: this })
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
       // insert the procuct
       dbOp = db.collection('products')
-        .insertOne(this)
+        .insertOne(this);
     }
     return dbOp
       .then(result => {console.log(result)})
@@ -30,7 +31,7 @@ class Product {
   }
 
   static fecthAll() {
-    const db = getDb()
+    const db = getDb();
     return db.collection('products')
       .find()
       .toArray()
