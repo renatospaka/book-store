6abcc0aa8eb82e29ab4f641ae817bb974b684706
Renato Spakauskas <renatospaka@gmail.com>
2020-09-10 15:38:21 -0300

    CRUD Pedido - mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -103,13 +103,13 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders()
-  .then(orders => {
-    res.render('shop/orders', {
-      path: '/orders',
-      pageTitle: 'Your Orders',
-      orders: orders
-    });
-  })
-  .catch(err => console.log('getOrders: ', err))
+  Order.find({ 'user.userId': req.user._id })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => console.log('getOrders: ', err));
 }
