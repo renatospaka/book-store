1d883d800c61d1e45bab78b4792e1de5946b2c4c
Renato Spakauskas <renatospaka@gmail.com>
2020-09-10 14:44:05 -0300

    CRUD Carrinho - mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -69,7 +69,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.deleteFromCart(prodId)
+  req.user.removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart')
     })
--- models/user.js
+++ models/user.js
@@ -46,6 +46,13 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 }
 
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
 // const mongodb = require('mongodb');
@@ -92,9 +99,7 @@ module.exports = mongoose.model('User', userSchema);
 //   }
 
 //   deleteFromCart(productId) {
-//     const updatedCartItems = this.cart.items.filter(item => {
-//       return item.productId.toString() !== productId.toString();
-//     });
+
     
 //     const db = getDb();
 //     return db.collection('users')
